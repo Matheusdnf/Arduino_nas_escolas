590743a1a664d88638a0b6ad5ae01c821e8f9111
Matheusdnf <matheusdiniz870@gmail.com>
2024-10-29 23:17:50 -0300

    correção de arquivos

--- semáforo/semaforo.py
+++ semáforo/semaforo.py
@@ -11,23 +11,20 @@ led_amarelo = arduino.get_pin('d:12:o')
 led_verde = arduino.get_pin('d:13:o')
 
 while True:
+    #Acender Led Verde
+    led_verde.write(1)
+    arduino.pass_time(5)
+    led_verde.write(0)
+    
+    #Acender Led Amarelo
+    led_amarelo.write(1)
+    arduino.pass_time(2)
+    led_amarelo.write(0)
+
     #Acender Led Vermelho
     led_vermelho.write(1)
     arduino.pass_time(5)
     led_vermelho.write(0)
 
-    #Acender Led Amarelo
-    led_amarelo.write(1)
-    arduino.pass_time(2)
-    led_amarelo.write(0)
 
-    #Acender Led Verde
-    led_verde.write(1)
-    arduino.pass_time(5)
-    led_verde.write(0)
-
-    #Acender Led Amarelo
-    led_amarelo.write(1)
-    arduino.pass_time(2)
-    led_amarelo.write(0)
     
